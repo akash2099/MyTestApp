2814c04243dee012e4f2d5542ebb07e09234eb91
Akash Manna <akashmanna2099@gmail.com>
2021-09-12 11:39:32 +0530

    yo

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // This is the main Activity
+        // Another comment
+        // Adding another comment for PR
     }
 }
